1ee5fdc2c3b73ae8c425003459d5ba190c41d5e2
PatrikJMelander <patrikjmelander@gmail.com>
2021-08-27 13:13:16 +0200

    One mock test done

---
+++ src/main/java/com/example/demobank/models/ApprovalDto.java
@@ -0,0 +1,11 @@
+package com.example.demobank.models;
+
+/**
+ * Created by Patrik Melander
+ * Date: 2021-08-27
+ * Time: 13:12
+ * Project: demoBank
+ * Copyright: MIT
+ */
+public class ApprovalDto {
+}
--- src/main/java/com/example/demobank/services/AccountService.java
+++ src/main/java/com/example/demobank/services/AccountService.java
@@ -4,7 +4,9 @@ import com.example.demobank.models.Account;
 import com.example.demobank.repositories.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
@@ -14,9 +16,13 @@ import java.util.List;
 public class AccountService {
     
     private final AccountRepository accountRepository;
+
+    RestTemplate restTemplate = new RestTemplate();
     
     public Account addAccount(String holder) {
-    
+
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://localhost:8080/risk/"+ holder, String.class);
+
         Account account = new Account(holder);
         System.out.println(account);
         Account saved = accountRepository.save(account);
